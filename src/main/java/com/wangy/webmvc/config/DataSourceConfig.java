11e211882d31a37436b1dee6cac0018fc279f423
wangy325 <wangy325@qq.com>
2020-03-24 01:08:03 +0800

    加注

--- src/main/java/com/wangy/webmvc/config/DataSourceConfig.java
+++ src/main/java/com/wangy/webmvc/config/DataSourceConfig.java
@@ -3,7 +3,10 @@ package com.wangy.webmvc.config;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -30,27 +33,44 @@ public class DataSourceConfig {
     private String username;
     @Value("${spring.datasource.password}")
     private String password;
+    @Value("${spring.datasource.schema}")
+    private String h2Schema;
+    @Value("${spring.datasource.data}")
+    private String h2Data;
 
 
     @Bean
-    @Profile("dev")
-    public DataSource embeddedDataSource() {
+    @Profile("dev0")
+    public DataSource embeddedDataSource_0() {
+        //以下配置使用Spring jdbc配置嵌入式数据库
+        //使用h2数据库默认配置(testdb,sa,empty)并运行指定脚本
+        //脚本可自动配置，无需指定
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            /*.addScript(h2Schema)
+            .addScript(h2Data)*/
+            .build();
+    }
+
+
+    @Bean
+    @Profile("dev1")
+    public DataSource embeddedDataSource_1() {
+        // 以下配置使用hikari连接池接入h2内存数据库
+        // TODO 脚本自动配置运行 ？
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(h2Url);
         hikariConfig.setDriverClassName(h2Driver);
         hikariConfig.setUsername(username);
         hikariConfig.setPassword(password);
         return new HikariDataSource(hikariConfig);
-        /*return new EmbeddedDatabaseBuilder()
-            .setType(EmbeddedDatabaseType.H2)
-            .addScript(h2Schema)
-            .addScript(h2Data)
-            .build();*/
     }
+
 
     @Bean
     @Profile("qa")
     public DataSource dataSource() {
+        // 简单的hikari数据库连接池配置
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(mysqlUrl);
         hikariConfig.setDriverClassName(mysqlDriver);
--- src/main/java/com/wangy/webmvc/config/JdbcConfig.java
+++ src/main/java/com/wangy/webmvc/config/JdbcConfig.java
@@ -1,15 +1,15 @@
 package com.wangy.webmvc.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
 
 /**
+ * 使用Spring JDBC提供的{@link JdbcTemplate}来减少Java JDBC的样板式代码（连接获取，异常处理，资源释放...）
+ *
  * @author wangy
  * @version 1.0
  * @date 2020/3/20 / 14:57
@@ -17,11 +17,14 @@ import javax.sql.DataSource;
 @Configuration
 public class JdbcConfig {
 
-    @Autowired
-    private DataSource dataSource;
+    private final DataSource dataSource;
+
+    public JdbcConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
-    public JdbcTemplate jdbcTemplate(){
+    public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource);
     }
 }
